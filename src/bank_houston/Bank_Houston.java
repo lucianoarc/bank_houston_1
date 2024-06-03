c94902ecac1deeb23eef00516c103a1c515d7cf5
lucia <lucia@Galaxy-Book-3>
2024-06-03 18:52:02 -0400

    bank_houston main carga inicial

---
+++ src/bank_houston/Bank_Houston.java
@@ -0,0 +1,14 @@
+
+package bank_houston;
+
+import bank_houston.Menu.Menu;
+
+public class Bank_Houston {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Bank Houston");
+        System.out.println("");
+        Menu.Menu();
+    }
+    
+}
